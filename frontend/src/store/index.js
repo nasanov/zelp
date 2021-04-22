1353ee866e78f52a6a5b4d22fa90094e905a973d
nasanov <asanoff.n@gmail.com>
2021-04-22 15:47:03 -0500

    Reux set up

---
+++ frontend/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+
+})
+
+// enhancer vaiable will be set to different store enhancers depending on if the Node environment is in development or production
+let enhancer;
+
+// in production engancer sohuld only apply thunk,
+// in dev env logger and redux dev tools also compose enhancer
+if (process.env.NODE_ENV === 'production') {
+	enhancer = applyMiddleware(thunk);
+} else {
+	const logger = require('redux-logger').default;
+	const composeEnhancer =
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	enhancer = composeEnhancer(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+	return createStore(rootReducer, preloadedState, enhancer)
+}
+
+export default configureStore
