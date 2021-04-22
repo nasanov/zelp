7114909c96196b36d7de832e61efb784348564d9
nasanov <asanoff.n@gmail.com>
2021-04-22 14:43:09 -0500

    handleValidationErrors()

---
+++ backend/utils/validation.js
@@ -0,0 +1,24 @@
+const { validationResult } = require('express-validator')
+// gathers the results of all the check middlewares that were run to determine which parts of the body are valid and invalid.
+
+// middleware for formatting errors from express-validator middleware
+const handleValidationError = (req, res, next) => {
+	const validationErrors = validationResult(req)
+
+	if (!validationErrors.isEmpty()) {
+		const errors = validationErrors
+			.array()
+			.map(error => `${error.msg}`)
+
+			const err = Error('Badd request.')
+			err.errors = errors;
+			err.status = 400;
+			err.title = 'Bad request.'
+			next(err)
+	}
+	next();
+}
+
+module.exports = {
+	handleValidationError
+}
