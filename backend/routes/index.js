fb85667fbc161cb9a5f71e1bb1a2c24311ba6aa4
nasanov <asanoff.n@gmail.com>
2021-04-22 11:11:35 -0500

    set up api routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router();
+const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
 
 router.get('/hello/world', (req, res) => {
 	// setting a cookie on the response with the name of XSRF-TOKEN to the value of the req.csrfToken method's return.
