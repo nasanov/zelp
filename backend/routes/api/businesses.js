963b83ca6fb550f9f6f837edaf9bccc67855df6a
nasanov <asanoff.n@gmail.com>
2021-04-27 18:54:29 -0500

    worked on routes

--- backend/routes/api/businesses.js
+++ backend/routes/api/businesses.js
@@ -11,24 +11,11 @@ router.get('/', asyncHandler(async (req,res) => {
 }))
 
 router.get('/:id', asyncHandler(async (req,res) => {
-	// get one business
-}))
-
-router.get('/:id/reviews', asyncHandler(async (req,res) => {
-	// get reviews for 1 business
-}))
-
-router.post('/:id/reviews', asyncHandler(async (req,res) => {
-	// add review for a business
-	const { review_text, rating, userId, business_id } = req.body;
-}))
-
-router.patch('/:id/reviews/:id', asyncHandler(async (req,res) => {
-	// edit review for a business
-}))
-
-router.delete('/:id/reviews/:id', asyncHandler(async (req,res) => {
-	// delete review for a business
+	const {id} = req.params;
+	// console.log(id)
+	let result = await Business.findByPk(id)
+	// console.log(result)
+	res.json(result)
 }))
 
 module.exports = router;
